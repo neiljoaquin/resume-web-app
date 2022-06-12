0355fd45d1db4bb2ade38ca32904968451ca6e38
Neil Joaquin <neil.joaquin@ortechnologies.com.au>
2022-06-12 20:07:05 +1000

    Fix UI

--- components/timeline/ORTechDescription.tsx
+++ components/timeline/ORTechDescription.tsx
@@ -8,8 +8,10 @@ export default function ORTechDescription() {
       date={"2019 July"}
       description={"Started working for OR Technologies as a Software Engineer"}
     >
-      <Text fontSize={"sm"}>Android and iOS apps:</Text>
-      <Text fontSize={"sm"} pl={"3"}>
+      <Text fontSize={"sm"} fontWeight={"bold"}>
+        Android and iOS apps:
+      </Text>
+      <Text fontSize={"sm"} pl={"3"} pb={"2"}>
         Apps were mainly used to interface with the nodes using technologies
         such as IR, BLE and NFC.
       </Text>
@@ -20,39 +22,65 @@ export default function ORTechDescription() {
 
       <Divider></Divider>
       <Box pt={"2"}>
-        <Text fontSize={"sm"}>Embedded Devices:</Text>
-        <Text fontSize={"sm"} pl={"3"}>
-          Organic Response&#39;s main product that is mainly used for IoT lighting. We also refer to them as &#34;nodes&#34;.
+        <Text fontSize={"sm"} fontWeight={"bold"}>
+          Embedded Devices:
         </Text>
-        <TechUsedComponent tech={"Tech used:"} techUsed={"C and Wirepas tech stack"} />
+        <Text fontSize={"sm"} pl={"3"} pb={"2"}>
+          Organic Response&#39;s main product that is mainly used for IoT
+          lighting. We also refer to them as &#34;nodes&#34;.
+        </Text>
+        <TechUsedComponent
+          tech={"Tech used:"}
+          techUsed={"C and Wirepas tech stack"}
+        />
       </Box>
 
       <Divider></Divider>
       <Box pt={"2"}>
-        <Text fontSize={"sm"}>CI/CD Pipelines:</Text>
-        <Text fontSize={"sm"} pl={"3"}>
-          The pipelines for embedded devices and mobile apps (Android and iOS) are used to automate development, testing and deployment.
+        <Text fontSize={"sm"} fontWeight={"bold"}>
+          CI/CD Pipelines:
         </Text>
-        <TechUsedComponent tech={"Tech used:"} techUsed={"Jenkins and Bitrise"} />
+        <Text fontSize={"sm"} pl={"3"} pb={"2"}>
+          The pipelines for embedded devices and mobile apps (Android and iOS)
+          are used to automate development, testing and deployment.
+        </Text>
+        <TechUsedComponent
+          tech={"Tech used:"}
+          techUsed={"Jenkins and Bitrise"}
+        />
       </Box>
 
       <Divider></Divider>
       <Box pt={"2"}>
-        <Text fontSize={"sm"}>IoT device:</Text>
-        <Text fontSize={"sm"} pl={"3"}>
-          The IoT device or as it call it, the gateway, is what we use as a middleman for our web app portal to able to communicate with our our nodes. 
+        <Text fontSize={"sm"} fontWeight={"bold"}>
+          IoT device:
         </Text>
-        <TechUsedComponent tech={"Tech used:"} techUsed={"Python and Rigado tech stack"} />
+        <Text fontSize={"sm"} pl={"3"} pb={"2"}>
+          The IoT device or as we call it, the gateway, is what we use as a
+          middleman for our web app portal to be able to communicate with our
+          nodes.
+        </Text>
+        <TechUsedComponent
+          tech={"Tech used:"}
+          techUsed={"Python and Rigado tech stack"}
+        />
       </Box>
 
       <Divider></Divider>
       <Box pt={"2"}>
-        <Text fontSize={"sm"}>Web Portal:</Text>
-        <Text fontSize={"sm"} pl={"3"}>
-          The IoT device or as it call it, the gateway, is what we use as a middleman for our web app portal to able to communicate with our our nodes. 
+        <Text fontSize={"sm"} fontWeight={"bold"}>
+          Web Portal:
         </Text>
-        <TechUsedComponent tech={"Tech used:"} techUsed={"Java, Spring Boot and Hibernate"} />
+        <Text fontSize={"sm"} pl={"3"} pb={"2"}>
+          The web app portal is like the central hub of the IoT system where all
+          of the node&#39;s data are collected. We can also use this to
+          interface with the nodes remotely.
+        </Text>
+        <TechUsedComponent
+          tech={"Tech used:"}
+          techUsed={"Java, Spring Boot and Hibernate"}
+        />
       </Box>
-    </TimelineCard> 
-  )
+    </TimelineCard>
+  );
 }
--- components/timeline/SamsungDescription.tsx
+++ components/timeline/SamsungDescription.tsx
@@ -10,8 +10,8 @@ export default function SamsungDescription() {
         "Worked for Samsung Research and Development Philippines as a Software Engineer"
       }
     >
-      <Text fontSize={"sm"}>Knox Configure Client Android app:</Text>
-      <Text fontSize={"sm"} pl={"3"}>
+      <Text fontSize={"sm"} fontWeight={"bold"}>Knox Configure Client Android app:</Text>
+      <Text fontSize={"sm"} pl={"3"} pb={"2"}>
         Knox Configure is a cloud-based service that enables enterprises to
         configure their company Samsung devices. The Android app works hand in
         hand with a Portal based web app to configure the customer&#39;s
@@ -24,8 +24,8 @@ export default function SamsungDescription() {
 
       <Divider></Divider>
       <Box pt={"2"}>
-        <Text fontSize={"sm"}>CI/CD pipeline:</Text>
-        <Text fontSize={"sm"} pl={"3"}>
+        <Text fontSize={"sm"} fontWeight={"bold"}>CI/CD pipeline:</Text>
+        <Text fontSize={"sm"} pl={"3"} pb={"2"}>
           The pipeline for the Android app is used to automate development and
           testing.
         </Text>
@@ -37,8 +37,8 @@ export default function SamsungDescription() {
 
       <Divider></Divider>
       <Box pt={"2"}>
-        <Text fontSize={"sm"}>Samsung Algo Challenge:</Text>
-        <Text fontSize={"sm"} pl={"3"}>
+        <Text fontSize={"sm"} fontWeight={"bold"}>Samsung Algo Challenge:</Text>
+        <Text fontSize={"sm"} pl={"3"} pb={"2"}>
           Web app that is used to facilitate coding competitions. 
         </Text>
         <TechUsedComponent
